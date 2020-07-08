90a7bdec1d5a11b1e5eb8dbb8dc41ff36bc7a128
Richard Raines <rrraines1703@me.com>
2020-07-08 10:31:49 -0400

    Completed CHP 19 Studio

--- CodingEventsDemo/Controllers/EventCategoryContoller.cs
+++ CodingEventsDemo/Controllers/EventCategoryContoller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Controllers
@@ -23,6 +24,33 @@ namespace CodingEventsDemo.Controllers
         {
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+            return View("Create", addEventCategoryViewModel);
+
         }
     }
 }
